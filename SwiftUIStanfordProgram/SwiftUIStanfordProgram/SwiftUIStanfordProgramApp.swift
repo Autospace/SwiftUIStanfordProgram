a7bc62ceddbedd592523ff965767d8ed15da011e
Aliaksei Mastounikau <aliaksei_mastounikau@epam.com>
2022-01-31 14:35:02 +0300

    Initial commit

---
+++ SwiftUIStanfordProgram/SwiftUIStanfordProgram/SwiftUIStanfordProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStanfordProgramApp.swift
+//  SwiftUIStanfordProgram
+//
+//  Created by Aliaksei Mastounikau on 31.01.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStanfordProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
