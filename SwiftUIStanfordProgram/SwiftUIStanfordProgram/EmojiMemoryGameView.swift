ccdca05dd584d6f529deb4e865c342a2cad70e18
MALX <mostovnikov.alex@gmail.com>
2022-06-29 19:55:59 +0300

    Replace deprecated .animation modifier with a new one

--- SwiftUIStanfordProgram/SwiftUIStanfordProgram/EmojiMemoryGameView.swift
+++ SwiftUIStanfordProgram/SwiftUIStanfordProgram/EmojiMemoryGameView.swift
@@ -142,7 +142,7 @@ struct CardView: View {
                     .opacity(0.5)
                 Text(card.content)
                     .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
-                    .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                    .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false), value: Angle.degrees(card.isMatched ? 360 : 0))
                     .font(Font.system(size: DrawingConstants.fontSize))
                     .scaleEffect(scale(thatFits: geometry.size))
             }
