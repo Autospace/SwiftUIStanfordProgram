acc9cf0f98b86ab55e94d4c46597aaf0009e1766
MALX <mostovnikov.alex@gmail.com>
2022-06-30 21:10:41 +0300

    Lecture 9: EmojiArt Drag and Drop Multithreading. Add EmojiArt project

---
+++ EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Aliaksei Mastounikau on 30.06.22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
